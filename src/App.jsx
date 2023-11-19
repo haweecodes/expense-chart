67f4f4ccef7be9e003cae136752798d6e6916c6c
haweecodes <haweesajib10@gmail.com>
2023-11-19 21:00:33 +0600

    chore: card component ui and layaout creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Card from './components/card';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Expense Chart</h1>
-      <section></section>
+      <section>
+        <Card></Card>
+      </section>
       <footer></footer>
     </div>
   );
---
+++ src/components/card/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <p className="card-title">Expenses</p>
+      <p>filter</p>
+      <p>chart</p>
+      <p>legend</p>
+    </div>
+  );
+};
+
+export default Card;
