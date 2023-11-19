4fcb6b3fc4a2dc21489625879a1636bf53ffb752
haweecodes <haweesajib10@gmail.com>
2023-11-19 20:35:31 +0600

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Expenses</h1>
+      <section></section>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
