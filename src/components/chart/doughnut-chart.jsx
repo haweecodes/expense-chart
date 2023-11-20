60a4f0c80fd4479eb7d8a33889f62530c59e6715
haweecodes <haweesajib10@gmail.com>
2023-11-20 21:53:49 +0600

    chore: rework the donut ui

--- src/components/chart/doughnut-chart.jsx
+++ src/components/chart/doughnut-chart.jsx
@@ -3,6 +3,26 @@ import { getColors } from "../../utils";
 import "./style.css";
 
 const DoughnutChart = ({ data, filterState }) => {
+  function generateConicGradient(timePeriod) {
+    const numericValues = Object.values(timePeriod).filter(
+      (value) => typeof value === "number"
+    );
+    const totalCount = numericValues.reduce((acc, value) => acc + value, 0);
+
+    let prevStart = 0;
+    const colorStops = Object.keys(timePeriod)
+      .filter((key) => key !== "period")
+      .map((value, index) => {
+        const percentage = (timePeriod[value] / totalCount) * 100;
+        const start = prevStart;
+        const end = start + percentage;
+        prevStart = end;
+
+        return `${getColors(index)} ${start}% ${end}%`;
+      });
+
+    return `conic-gradient(${colorStops.join(", ")})`;
+  }
 
   useEffect(() => {
     if (data) {
@@ -20,27 +40,11 @@ const DoughnutChart = ({ data, filterState }) => {
         (value) => typeof value === "number"
       );
       const totalCount = numericValues.reduce((acc, value) => acc + value, 0);
-
-      let startAngle = 0;
-
-      Object.keys(timePeriod)
-        .filter((key) => key !== "period")
-        .map((value, index) => {
-          const percentage = (timePeriod[value] / totalCount) * 100;
-
-          const segmentElement = document.createElement("div");
-          segmentElement.classList.add("portion-block");
-          segmentElement.style.transform = `rotate(${startAngle}deg)`;
-          segmentElement.style.background = getColors(index);
-
-          startAngle += (percentage / 100) * 360;
-
-          container.appendChild(segmentElement);
-        });
+      container.style.background = generateConicGradient(timePeriod);
 
       const centerDiv = document.createElement("div");
       centerDiv.classList.add("center");
-      centerDiv.innerHTML = `$ ${totalCount}`;
+      centerDiv.innerHTML = `$ ${totalCount.toFixed(2)}`;
       container.appendChild(centerDiv);
     }
   }, [data, filterState]);
